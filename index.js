2338267b387561d4ffa4af3b7beb478e7656f10e
123a456b789c <50078004+123a456b789c@users.noreply.github.com>
2024-03-06 15:47:16 +0100

    Fixed viewer

--- index.js
+++ index.js
@@ -31,57 +31,6 @@ app.get('/rawList/:list', async (req, res) => {
     res.sendFile(path.join(listDir, req.params.list));
 })
 
-
-app.get('/:jwt/:lang', async (req, res) => {
-    const lang = req.params.lang;
-    var startTime = Date.now();
-    var decoded = ""
-    try {
-        decoded = jwt.verify(req.params.jwt, process.env.JWT_SECRET);
-    } catch(err) {
-        if (req.params.jwt != "guest") {
-            console.log("[MEGNYITÁS] Hiba a token dekódolása közben. ", req.params.jwt)
-            res.render('forbidden', { error: 'A token nem érvényes.' });
-            return;
-        }
-    }
-    var listDir = path.join(__dirname, 'lists');
-    var files = fs.readdirSync(listDir);
-    switch (lang) {
-        case "hu":
-            var lists = "<table><tr><th>Lista neve</th><th></th><th></th><th></th></tr>";
-            break;
-        case "en":
-            var lists = "<table><tr><th>List</th><th></th><th></th><th></th></tr>";
-            break;
-        default:
-            var lists = "<table><tr><th>Lista neve</th><th></th><th></th><th></th></tr>";
-            break;
-    }
-    files.forEach(file => {
-        switch (lang) {
-            case "hu":
-                lists += `<tr><td><a href="/rawList/${file}">${file.replace(".txt","")}</a></td><td><a href="/loadList/${file}/${lang}">Kártya</a></td><td><a href="/writeList/${file}/${lang}">Írás</a></td><td><a href="/viewList/${file}">Nézegető</a></td></tr>`
-                break;
-            case "en":
-                lists += `<tr><td><a href="/rawList/${file}">${file.replace(".txt","")}</a></td><td><a href="/loadList/${file}/${lang}">Cards</a></td><td><a href="/writeList/${file}/${lang}">Write</a></td><td><a href="/viewList/${file}">Viewer</a></td></tr>`
-                break;
-            default:
-                lists += `<tr><td><a href="/rawList/${file}">${file.replace(".txt","")}</a></td><td><a href="/loadList/${file}/${lang}">Kártya</a></td><td><a href="/writeList/${file}/${lang}">Írás</a></td><td><a href="/viewList/${file}">Nézegető</a></td></tr>`
-                break;
-        }
-    });
-    var stopTime = Date.now();
-    var time = stopTime - startTime;
-    if (req.params.jwt == "guest") {
-        console.log("[MEGNYITÁS] A felhasználó megnyitotta a listákat.", "guest");
-        fs.appendFileSync('log.txt', `[MEGNYITÁS] A felhasználó megnyitotta a listákat. (guest)  ${new Date().toISOString()} ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}\n`);
-    } else {
-        fs.appendFileSync('log.txt', `[MEGNYITÁS] A felhasználó megnyitotta a listákat. (${decoded.username})  ${new Date().toISOString()} ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}\n`);
-    }
-    res.render(lang + '-lists', { lists: lists, time: time });
-});
-
 app.get('/loadList/:list/:lang', async (req, res) => {
     var startTime = Date.now();
     var listDir = path.join(__dirname, 'lists');
@@ -209,6 +158,56 @@ app.get('/viewList/:list/:lang', async (req, res) => {
     res.render(req.params.lang + '-viewer', {listname: req.params.list.replace(".txt",""), time: time, questionArray: questionArray, answerArray: answerArray });
 })
 
+
+app.get('/:jwt/:lang', async (req, res) => {
+    const lang = req.params.lang;
+    var startTime = Date.now();
+    var decoded = ""
+    try {
+        decoded = jwt.verify(req.params.jwt, process.env.JWT_SECRET);
+    } catch(err) {
+        if (req.params.jwt != "guest") {
+            console.log("[MEGNYITÁS] Hiba a token dekódolása közben. ", req.params.jwt)
+            res.render('forbidden', { error: 'A token nem érvényes.' });
+            return;
+        }
+    }
+    var listDir = path.join(__dirname, 'lists');
+    var files = fs.readdirSync(listDir);
+    switch (lang) {
+        case "hu":
+            var lists = "<table><tr><th>Lista neve</th><th></th><th></th><th></th></tr>";
+            break;
+        case "en":
+            var lists = "<table><tr><th>List</th><th></th><th></th><th></th></tr>";
+            break;
+        default:
+            var lists = "<table><tr><th>Lista neve</th><th></th><th></th><th></th></tr>";
+            break;
+    }
+    files.forEach(file => {
+        switch (lang) {
+            case "hu":
+                lists += `<tr><td><a href="/rawList/${file}">${file.replace(".txt","")}</a></td><td><a href="/loadList/${file}/${lang}">Kártya</a></td><td><a href="/writeList/${file}/${lang}">Írás</a></td><td><a href="/viewList/${file}/${lang}">Nézegető</a></td></tr>`
+                break;
+            case "en":
+                lists += `<tr><td><a href="/rawList/${file}">${file.replace(".txt","")}</a></td><td><a href="/loadList/${file}/${lang}">Cards</a></td><td><a href="/writeList/${file}/${lang}">Write</a></td><td><a href="/viewList/${file}/${lang}">Viewer</a></td></tr>`
+                break;
+            default:
+                lists += `<tr><td><a href="/rawList/${file}">${file.replace(".txt","")}</a></td><td><a href="/loadList/${file}/${lang}">Kártya</a></td><td><a href="/writeList/${file}/${lang}">Írás</a></td><td><a href="/viewList/${file}/${lang}">Nézegető</a></td></tr>`
+                break;
+        }
+    });
+    var stopTime = Date.now();
+    var time = stopTime - startTime;
+    if (req.params.jwt == "guest") {
+        console.log("[MEGNYITÁS] A felhasználó megnyitotta a listákat.", "guest");
+        fs.appendFileSync('log.txt', `[MEGNYITÁS] A felhasználó megnyitotta a listákat. (guest)  ${new Date().toISOString()} ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}\n`);
+    } else {
+        fs.appendFileSync('log.txt', `[MEGNYITÁS] A felhasználó megnyitotta a listákat. (${decoded.username})  ${new Date().toISOString()} ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}\n`);
+    }
+    res.render(lang + '-lists', { lists: lists, time: time });
+});
 
 app.listen(port, () => {
     console.log(`Kész! (port: ${port})`);
